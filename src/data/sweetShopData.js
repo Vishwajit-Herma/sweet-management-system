127ebb57a48790f44d393956e445bc46004fd095
Vishwajit Herma <vishuherma@gmail.com>
2025-07-17 14:22:00 +0530

    add dummy data

--- src/data/sweetShopData.js
+++ src/data/sweetShopData.js
@@ -1,1 +1,31 @@
-export const sweets = [];
+export let sweets = [
+  {
+    id: 1,
+    name: "Kaju Katli",
+    category: "Dry Fruit",
+    price: 25,
+    quantity: 50,
+  },
+  {
+    id: 2,
+    name: "Rasgulla",
+    category: "Bengali",
+    price: 15,
+    quantity: 100,
+  },
+  {
+    id: 3,
+    name: "Ladoo",
+    category: "Traditional",
+    price: 10,
+    quantity: 200,
+  },
+  {
+    id: 4,
+    name: "Chocolate Barfi",
+    category: "Fusion",
+    price: 30,
+    quantity: 75,
+  },
+];
+
